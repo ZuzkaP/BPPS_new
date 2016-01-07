6a987b14dda3ed51631999174551b897358ea860
Zuzka <zuzka.papalova@gmail.com>
2016-01-07 09:40:26 +0100

    Export by filter

--- BPPS/Controllers/feedbacksController.cs
+++ BPPS/Controllers/feedbacksController.cs
@@ -15,6 +15,9 @@ using System.IO;
 using Rotativa;
 using System.Web.UI;
 using Postal;
+using System.Globalization;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
 
 namespace BPPS.Controllers
 {
@@ -22,10 +25,12 @@ namespace BPPS.Controllers
     public class feedbacksController : Controller
     {
         private Entities db = new Entities();
-
+        DateTime result;
+        DateTime initiated;
+        DataTable dtPDF = new DataTable();
         [Authorize(Roles = "admin, siemens")]
         // GET: feedbacks
-        public ActionResult Index(string project_name, string buisness_partner, int? page)
+        public ActionResult Index(string project_name, string buisness_partner, int? page, string Feedback_initialized, string Feedback_received)
         {
             /*var feedbacks = from m in db.feedbacks select m;
             var projects = from m in db.Projects.ToList()
@@ -49,12 +54,31 @@ namespace BPPS.Controllers
             {
                 feedback = feedback.Where(fe => fe.AspNetUsers.FirstName.ToUpper() == buisness_partner.ToUpper()).ToList();
             }
+
+            if (!String.IsNullOrEmpty(Feedback_initialized))
+            {
+                result = DateTime.ParseExact(Feedback_initialized, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+                
+                feedback = feedback.Where(fe => fe.initiated.Value.Date.ToShortDateString() == result.Date.ToShortDateString()).ToList();
+                
+            }
+
+            if (!String.IsNullOrEmpty(Feedback_received))
+            {
+                result = DateTime.ParseExact(Feedback_received, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+
+                feedback = feedback.Where(fe => fe.received.Value.Date.ToShortDateString() == result.Date.ToShortDateString()).ToList();
+               
+            }
+
             if (!String.IsNullOrEmpty(buisness_partner) || !String.IsNullOrEmpty(project_name))
             {
+                Session["feedbackList"] = feedback;
                 return View(feedback.ToPagedList(pageNumber, pageSize));
             }
 
-            return View(db.feedbacks.OrderByDescending(f => f.received).ToPagedList(pageNumber, pageSize));
+            Session["feedbackList"] = feedback;
+            return View(feedback.OrderByDescending(f => f.received).ToPagedList(pageNumber, pageSize));
         }
 
         [Authorize(Roles = "admin, siemens")]
@@ -352,7 +376,7 @@ namespace BPPS.Controllers
         public ActionResult ExportData(int? id)
         {
             var datasource = db.feedback_questions.Where(f => f.feedback_id == id).Select(f => new { f.questions.question, f.result, f.comment }).ToList();
-
+           
             string FilePath = Server.MapPath("~/Temp/");
             //string FileName = "test.txt";
 
@@ -378,9 +402,9 @@ namespace BPPS.Controllers
         public ActionResult ExportData2()
         {
             var datasource = db.feedback_questions.OrderBy(f => f.feedbacks.Projects.name).ThenBy(f => f.feedbacks.AspNetUsers.Email).Select(f => new { f.feedbacks.Projects.name, f.feedbacks.AspNetUsers.Email, f.questions.question, f.result, f.comment }).ToList();
-
+            var datasource2 = (List<feedbacks>)Session["feedbackList"];
             GridView gv = new GridView();
-            gv.DataSource = datasource;
+            gv.DataSource = datasource2;
             gv.DataBind();
             Response.ClearContent();
             Response.Buffer = true;
@@ -414,5 +438,79 @@ namespace BPPS.Controllers
                 FileName = "Report_all.pdf"
             };
         }
+      
+        //protected void btnPDF_Click(object sender, ImageClickEventArgs e)
+        //{
+        //    DataTable dtn = new DataTable();
+        //    dtn = GetDataTable();
+        //    dtPDF = dtn.Copy();
+        //    for (int i = 0; i <= dtn.Rows.Count - 1; i++)
+        //    {
+        //        ExportToPdf(dtPDF);
+        //    }
+        //}
+
+        //public void ExportToPdf(DataTable myDataTable)
+        //{
+        //    Document pdfDoc = new Document(PageSize.A4, 10, 10, 10, 10);
+        //    try
+        //    {
+        //        PdfWriter.GetInstance(pdfDoc, System.Web.HttpContext.Current.Response.OutputStream);
+        //        pdfDoc.Open();
+        //        Chunk c = new Chunk("" + System.Web.HttpContext.Current.Session["CompanyName"] + "", FontFactory.GetFont("Verdana", 11));
+        //        Paragraph p = new Paragraph();
+        //        p.Alignment = Element.ALIGN_CENTER;
+        //        p.Add(c);
+        //        pdfDoc.Add(p);
+        //        string clientLogo = Server.MapPath(".") + "/logo/tpglogo.jpg";
+        //        string imageFilePath = Server.MapPath(".") + "/logo/tpglogo.jpg";
+        //        iTextSharp.text.Image jpg = iTextSharp.text.Image.GetInstance(imageFilePath);
+        //        //Resize image depend upon your need   
+        //        jpg.ScaleToFit(80f, 60f);
+        //        //Give space before image   
+        //        jpg.SpacingBefore = 0f;
+        //        //Give some space after the image   
+        //        jpg.SpacingAfter = 1f;
+        //        jpg.Alignment = Element.HEADER;
+        //        pdfDoc.Add(jpg);
+        //        Font font8 = FontFactory.GetFont("ARIAL", 7);
+        //        DataTable dt = myDataTable;
+        //        if (dt != null)
+        //        {
+        //            //Craete instance of the pdf table and set the number of column in that table  
+        //            PdfPTable PdfTable = new PdfPTable(dt.Columns.Count);
+        //            PdfPCell PdfPCell = null;
+        //            for (int rows = 0; rows < dt.Rows.Count; rows++)
+        //            {
+        //                for (int column = 0; column < dt.Columns.Count; column++)
+        //                {
+        //                    PdfPCell = new PdfPCell(new Phrase(new Chunk(dt.Rows[rows][column].ToString(), font8)));
+        //                    PdfTable.AddCell(PdfPCell);
+        //                }
+        //            }
+        //            //PdfTable.SpacingBefore = 15f; // Give some space after the text or it may overlap the table            
+        //            pdfDoc.Add(PdfTable); // add pdf table to the document   
+        //        }
+        //        pdfDoc.Close();
+        //        Response.ContentType = "application/pdf";
+        //        Response.AddHeader("content-disposition", "attachment; filename= SampleExport.pdf");
+        //        System.Web.HttpContext.Current.Response.Write(pdfDoc);
+        //        Response.Flush();
+        //        Response.End();
+        //        //HttpContext.Current.ApplicationInstance.CompleteRequest();  
+        //    }
+        //    catch (DocumentException de)
+        //    {
+        //        System.Web.HttpContext.Current.Response.Write(de.Message);
+        //    }
+        //    catch (IOException ioEx)
+        //    {
+        //        System.Web.HttpContext.Current.Response.Write(ioEx.Message);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        System.Web.HttpContext.Current.Response.Write(ex.Message);
+        //    }
+        //}
     }
 }
